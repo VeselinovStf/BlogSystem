9f3a7587c62fed7d642e5c069a38e3a79b599e48
VeselinovLP <veselinovStf@gmail.com>
2019-05-20 13:55:53 +0300

    Get all Blog Post and Display them is done.

---
+++ src/BS.WEB.ViewModels/ViewComponents/BlogPostViewComponentModel.cs
@@ -0,0 +1,13 @@
+﻿using BS.WEB.ViewModels.BlogPost;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BS.WEB.ViewModels.ViewComponents
+{
+   public class BlogPostViewComponentModel
+    {
+        public IEnumerable<BlogPostViewModel> Posts { get; set; }
+        public bool IsPublic { get; set; }
+    }
+}
---
+++ src/BS.Web/ViewComponents/BlogPost/BlogPostSetViewComponent.cs
@@ -0,0 +1,46 @@
+﻿using BS.Identity.Manager.SignInManager.Wrapper.Abstract;
+using BS.Identity.Models;
+using BS.WEB.ViewModels.BlogPost;
+using BS.WEB.ViewModels.ViewComponents;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BS.Web.ViewComponents.BlogPost
+{
+    public class BlogPostSetViewComponent : ViewComponent
+    {
+        private readonly ISignInManagerWrapper<BaseIdentityUser> signInManager;
+
+        public BlogPostSetViewComponent(ISignInManagerWrapper<BaseIdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(IEnumerable<BlogPostViewModel>  model)
+        {
+            if (ModelState.IsValid)
+            {
+                BlogPostViewComponentModel returnModel = new BlogPostViewComponentModel()
+                {
+                    Posts = model
+                };
+
+                if (this.signInManager.IsSignedIn(HttpContext.User))
+                {
+                    returnModel.IsPublic = false;
+                }
+                else
+                {
+                    returnModel.IsPublic = true;
+                }
+
+                return View("BlogPostSet", returnModel);
+            }
+
+            return View();
+
+        }
+    }
+}
